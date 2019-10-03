e588ac04e8ab5592ded193b0a990556d3e50f24e
Jonathan Garton <jon.garton94@gmail.com>
2019-10-03 02:10:45 +0000

    completed part 2

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -1,4 +1,21 @@
 class StarsController < ApplicationController
   def index
+    @stars = Star.all
   end
+  def new
+    @star = Star.new
+  end
+
+  def create
+    Star.create(star_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def star_params
+    params.require(:star).permit(:name, :desription)
+  end
+
 end
+
